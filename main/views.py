89d0a9c42c069fa42f2c7841085742c0231cdfac
cichyp648 <cichyp648@gmail.com>
2025-05-24 14:52:27 +0200

    random movie on index

--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
 # Plik do definiowania widoków, które są renderowane za pomocą szablonizatora Jinja oraz wyświetlane w przeglądarce
 from django.shortcuts import render
+from movies.models import Movie
+import random
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    movies = Movie.objects.all()
+    movie = random.choice(movies) if movies else None
+    return render(request, 'main/index.html', {'random_movie': movie})
 
 
 def about(request):
